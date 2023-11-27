b7d529306bc4d254f062d083be007ee0863b2635
baekseungchan1117 <bsc7386@gmail.com>
2023-11-27 22:33:31 +0900

    로그아웃 구현하기

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,26 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link, useNavigate} from 'react-router-dom'
+import {Container,Nav,Navbar} from 'react-bootstrap';
+import { useSelector } from "react-redux"
+import firebase from '../firebase';
 
 
 const Heading = () => {
+  const user = useSelector((state) => state.user)
+  const navigate = useNavigate();
+
+  const LogoutHandler = () => {
+    firebase.auth().signOut();
+    navigate("/")
+  }
+
   return (
 
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Community</Navbar.Brand>
+        <Navbar.Brand href="/">Community</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
               <Link to="/" style={{color: "white", textDecoration: "none", marginRight: "10px"}}>home</Link>
               <Link to="/upload" style={{color: "white", textDecoration: "none", marginRight: "10px"}}>upload</Link>
-              <Link to="/login" style={{color: "white", textDecoration: "none", marginRight: "10px"}}>login</Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+        <Navbar.Collapse className='justify-content-end'>
+          {user.accessToken  ? ( 
+            <Navbar.Text style={{color: "white", cursor:"pointer"}} onClick={() => LogoutHandler()}>Logout</Navbar.Text>
+          ) : (<Link to="/login" style={{color: "white", textDecoration: "none"}}>login</Link>)}
+        </Navbar.Collapse>
+      </Container> 
     </Navbar>
 )}
 
